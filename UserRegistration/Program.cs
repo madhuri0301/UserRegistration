54812c7de582b7c3991340f97dd082e99e9ee678
Madhuri <harshupatil0301@gmail>
2021-06-18 19:38:42 +0530

    [Madhuri]Add : Validating Email Of User

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -6,15 +6,26 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
+            //class and it's object
             ValidateUserRegistration validateUserRegistration = new ValidateUserRegistration();
+
+            //firstname
             Console.WriteLine("Enter your first name : ");
             string firstName = Console.ReadLine();
             bool fNameResult = validateUserRegistration.ValidateFirstName(firstName); //Validating First Name Of User
             validateUserRegistration.PrintResult(fNameResult); // Printing first Name Of User
+
+            //lastname
             Console.WriteLine("Enter your last name : ");
             string lastName = Console.ReadLine();
             bool lNameResult = validateUserRegistration.ValidateFirstName(lastName); //Validating Last Name Of User
             validateUserRegistration.PrintResult(lNameResult); //Printing Last Name Of User
+
+            //emailId
+            Console.WriteLine("Enter your email : ");
+            string email = Console.ReadLine();
+            bool emailResult = validateUserRegistration.ValidateEmail(email); //Validating FisrtName Of User
+            validateUserRegistration.PrintResult(emailResult); //Print Email Of User
         }
     }
 }
