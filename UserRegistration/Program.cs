584dffaa372e9221563a6f60d407d33fb8ae5f2d
Madhuri <harshupatil0301@gmail>
2021-06-18 18:59:24 +0530

    [Madhuri]Add : Validating Last Name Of User

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -9,9 +9,12 @@ namespace UserRegistration
             ValidateUserRegistration validateUserRegistration = new ValidateUserRegistration();
             Console.WriteLine("Enter your first name : ");
             string firstName = Console.ReadLine();
-            bool fNameResult = validateUserRegistration.ValidateFirstName(firstName);
-            validateUserRegistration.PrintResult(fNameResult);
-
+            bool fNameResult = validateUserRegistration.ValidateFirstName(firstName); //Validating First Name Of User
+            validateUserRegistration.PrintResult(fNameResult); // Printing first Name Of User
+            Console.WriteLine("Enter your last name : ");
+            string lastName = Console.ReadLine();
+            bool lNameResult = validateUserRegistration.ValidateFirstName(lastName); //Validating Last Name Of User
+            validateUserRegistration.PrintResult(lNameResult); //Printing Last Name Of User
         }
     }
 }
